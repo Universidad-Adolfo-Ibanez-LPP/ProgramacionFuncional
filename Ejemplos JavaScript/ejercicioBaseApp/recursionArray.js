d9cfc42b39338000b18a0e4e188168ce854a4260
Justo Miguel Vargas <justomiguelvargas@gmail.com>
2022-06-15 09:23:48 -0400

    Agregado funciones

--- Ejemplos JavaScript/ejercicioBaseApp/recursionArray.js
+++ Ejemplos JavaScript/ejercicioBaseApp/recursionArray.js
@@ -1,14 +1,23 @@
-
+/**
+ * Esta funcion encuentra todos los caminos posibles
+ * moviendose unicamente para la derecha y/o abajo
+ *
+ * @param mat matriz con valores
+ * @param path camino posible
+ * @param i pos de fila
+ * @param j pos de columna
+ */
 function findPaths( mat, path,  i, j) {
     // base case
     if (mat == null || mat.length == 0) {
         return;
     }
 
+    //obtengo los extremos de fila y columna
     let M = mat.length;
     let N = mat[0].length;
 
-    // if the last cell is reached, print the route
+    // Si llegue al final imprimo el path
     if (i == M - 1 && j == N - 1) {
         path.push(mat[i][j]);
         console.log(path);
@@ -16,15 +25,15 @@ function findPaths( mat, path,  i, j) {
         return;
     }
 
-    // include the current cell in the path
-    path.push();
+    // meto la pos actual como parte del camino
+    path.push(mat[i][j]);
 
-    // move right
+    // muevo un lugar a la derecha
     if ((i >= 0 && i < M && j + 1 >= 0 && j + 1 < N)) {
         findPaths(mat, path, i, j + 1);
     }
 
-    // move down
+    // muevo un lugar abajo
     if ((i + 1 >= 0 && i + 1 < M && j >= 0 && j < N)) {
         findPaths(mat, path, i + 1, j);
     }
@@ -39,7 +48,7 @@ let mat = [ [ 1, 2, 3 ],
             [ 4, 5, 6 ],
             [ 7, 8, 9 ] ];
 
-let path =  [];
+var path =  [];
 
 // start from `(0, 0)` cell
 let x = 0, y = 0;
